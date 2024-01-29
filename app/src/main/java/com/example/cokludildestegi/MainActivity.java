f5812b6b0d70c01da70ae459c032b92c806f4c19
samet.uyanik <samet.uyanik2756@gmail.com>
2024-01-29 11:38:22 +0300

    Android Language Support

---
+++ app/src/main/java/com/example/cokludildestegi/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.cokludildestegi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //PROJE KISMINDAN VALUES-EN OLUŞTURDUK TASARIMIMIZA DİL DESTEĞİ EKLEDİK.
+        //HANGİ DİLİ EKLEMEK İSTERSEK VALUES-(DİL) YAPARAK GERÇEKLEŞTİREBİLİRSİNİZ.
+
+    }
+}
